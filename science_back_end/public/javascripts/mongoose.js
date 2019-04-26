c70f24c1566ba40588876debd0cb833cf2106960
Carlo Jacobs <carlojacobs91@gmail.com>
2019-04-26 14:30:34 +0200

    Update mongoose.js

--- science_back_end/public/javascripts/mongoose.js
+++ science_back_end/public/javascripts/mongoose.js
@@ -4,16 +4,11 @@
 
 const mongoose = require('mongoose');
 
-// Creating a mongoose conection
-/*mongoose.connect('mongodb://carlo:Dittoenbram1234@carlo-shard-00-00-nwaxe.mongodb.net:27017,carlo-shard-00-01-nwaxe.mongodb.net:27017,carlo-shard-00-02-nwaxe.mongodb.net:27017/worldofscience?ssl=true&replicaSet=carlo-shard-0&authSource=admin', {
-  useNewUrlParser: true
-});*/
-
 const dbUrl = 'mongodb://188.166.0.103:27017/worldofscience';
 mongoose.connect(dbUrl, {
 	auth: { authSource: "admin" },
 	user: 'carlo',
-  pass: 'Dittoenbram1234',
+  pass: 'Nice try',
 	useNewUrlParser: true
 });
 
